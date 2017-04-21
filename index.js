ff12998a574a46f3f8946327123cd85f93c6cd85
Matt Walters <mattwalters5@gmail.com>
2017-04-20 22:56:52 -0400

    removing loop flags from command, to let them be picked up from process.env by loop itself

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ module.exports = function (command) {
 
   folders.unshift(process.cwd());
 
+  // remove loop flags, and let loop pick them up from process.env
+  ['--exclude', '--exclude-only', '--include', '--include-only'].forEach((flag) => {
+    const flagIndex = command.indexOf(flag);
+    if (flagIndex > -1) {
+      command = command.substring(0, flagIndex);
+    }
+  });
+
   loop({
     command: command,
     directories: folders
