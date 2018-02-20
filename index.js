b8457ca31375a32cd6ccc69d4030529b2c766616
5ebastianMeier <sebastian.meier@5minds.de>
2018-02-20 19:32:32 +0100

    error code works

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const debug = require('debug')('@essential-projects/meta-loop');
+const debug = require('debug')('meta-loop');
 const getMetaFile = require('get-meta-file');
-const loop = require('@essential-projects/loop');
+const loop = require('loop');
 const path = require('path');
 const util = require('util');
 
@@ -13,10 +13,13 @@ module.exports = function (command) {
   const projects = meta.projects;
   const folders = Object.keys(projects).map((folder) => { return path.resolve(folder); });
 
+  const exitOnError = process.argv.indexOf('--exit-on-error') >= 0;
+  const exitOnAggregatedError = process.argv.indexOf('--exit-on-aggregated-error') >= 0;
+
   folders.unshift(process.cwd());
 
   // remove loop flags, and let loop pick them up from process.env
-  ['--exclude', '--exclude-only', '--include', '--include-only', '--exit-on-error', '--exit-on-aggregated-errors'].forEach((flag) => {
+  ['--exclude', '--exclude-only', '--include', '--include-only'].forEach((flag) => {
     const flagIndex = command.indexOf(flag);
     if (flagIndex > -1) {
       command = command.substring(0, flagIndex);
@@ -25,6 +28,13 @@ module.exports = function (command) {
 
   loop({
     command: command,
-    directories: folders
+    directories: folders,
+    exitOnError: exitOnError,
+    exitOnAggregatedError: exitOnAggregatedError,
+  }, (errorOccured) => {
+    if (exitOnAggregatedError && errorOccured) {
+      console.log('an error occured during loop execution - exiting process');
+      process.exit(1);
+    }
   });
 };
