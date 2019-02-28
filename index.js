12cc56132bc506808f1edc0204c056b1d7e12b99
Kelly Mills <Kelly.Mills@microsoft.com>
2019-02-28 03:38:26 -0500

    add recursive feature

--- index.js
+++ index.js
@@ -8,18 +8,30 @@ const path = require('path');
 const util = require('util');
 
 module.exports = function (command) {
-  
-  const meta = getMetaFile({ confirmInMetaRepo: true });
-  const projects = meta.projects;
-  const folders = Object.keys(projects).map((folder) => { return path.resolve(folder); });
-
+ 
+  function getChildFolders(parentFolder, recursive) {
+    const meta = getMetaFile({confirmInMetaRepo: true});
+    const projects = meta.projects; 
+    let childFolders = Object.keys(projects).map((folder) => { return path.resolve(folder); });
+    if (recursive) {
+      childFolders = childFolders.reduce(function (folders, folder) {
+        process.chdir(folder);
+        if (fs.existsSync('.meta')) {
+          folders = folders.concat(getChildFolders(folder,recursive));
+        }
+        return folders;
+      }, childFolders);
+      process.chdir(parentFolder);
+    }
+    return childFolders;
+  }
+  const folders = getChildFolders(process.cwd(),process.argv.indexOf('--recursive') >= 0);
   const exitOnError = process.argv.indexOf('--exit-on-error') >= 0;
   const exitOnAggregateError = process.argv.indexOf('--exit-on-aggregated-error') >= 0;
-
+  
   folders.unshift(process.cwd());
-
   // remove loop flags, and let loop pick them up from process.env
-  ['--exclude', '--exclude-only', '--include', '--include-only'].forEach((flag) => {
+  ['--exclude', '--exclude-only', '--include', '--include-only', '--recursive'].forEach((flag) => {
     const flagIndex = command.indexOf(flag);
     if (flagIndex > -1) {
       command = command.substring(0, flagIndex);
@@ -32,6 +44,7 @@ module.exports = function (command) {
     exitOnError: exitOnError,
     exitOnAggregateError: exitOnAggregateError,
   });
+  
 };
 
 module.exports.register = (program) => {
