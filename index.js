619c14169fe6f804e614a0877adb1934745b2591
Matt Walters <github@iammattwalters.com>
2019-06-11 13:26:28 -0500

    chore: standardizing test, linting, and commit messages

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const loop = require('loop');
 const path = require('path');
 const util = require('util');
 
-module.exports = function (command) {
-  
+module.exports = function(command) {
   const meta = getMetaFile({ confirmInMetaRepo: true });
   const projects = meta.projects;
-  const folders = Object.keys(projects).map((folder) => { return path.resolve(folder); });
+  const folders = Object.keys(projects).map(folder => {
+    return path.resolve(folder);
+  });
 
   const exitOnError = process.argv.indexOf('--exit-on-error') >= 0;
   const exitOnAggregateError = process.argv.indexOf('--exit-on-aggregated-error') >= 0;
@@ -19,7 +20,7 @@ module.exports = function (command) {
   folders.unshift(process.cwd());
 
   // remove loop flags, and let loop pick them up from process.env
-  ['--exclude', '--exclude-only', '--include', '--include-only'].forEach((flag) => {
+  ['--exclude', '--exclude-only', '--include', '--include-only'].forEach(flag => {
     const flagIndex = command.indexOf(flag);
     if (flagIndex > -1) {
       command = command.substring(0, flagIndex);
@@ -34,10 +35,6 @@ module.exports = function (command) {
   });
 };
 
-module.exports.register = (program) => {
-
-  program
-    .command('exec', 'execute a command against meta repo and child repo dirs')
-    .alias('loop')
-
-}
+module.exports.register = program => {
+  program.command('exec', 'execute a command against meta repo and child repo dirs').alias('loop');
+};
